905f6dc16b464732718b9b5c0183ebb2da1417fd
Vendula Konečná <VendaKonecna@gmail.com>
2025-06-28 15:51:21 +0200

    pridani komponenty Drink

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -10,7 +10,22 @@ export const Menu = () => {
                 Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
             </p>
             <div className="drinks-list">
-                <Drink />
+                <Drink
+                    id={0}
+                    name="Romano"
+                    ordered={false}
+                    image="http://localhost:4000/assets/cups/romano.png"
+                    layers={[
+                        {
+                        color: '#fbdf5b',
+                        label: 'citrón',
+                        },
+                        {
+                        color: '#613916',
+                        label: 'espresso',
+                        },
+                    ]}
+                />
             </div>
 
             <div className="order-detail">
