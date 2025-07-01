4d721141fdf40175dd169447d146a20060393de8
Vendula Konečná <VendaKonecna@gmail.com>
2025-07-01 18:48:29 +0200

    pridani komponent Order, OrderItem

--- src/pages/order.jsx
+++ src/pages/order.jsx
@@ -4,40 +4,17 @@ import './index.css';
 import './order.css';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
+import { Order } from '../components/Order';
+
+const response = await fetch('http://localhost:4000/api/drinks?filter=ordered:eq:true&select=id,name,image')
+const data = await response.json()
+console.log(data)
 
 document.querySelector('#root').innerHTML = render(
   <div className="page">
     <div className="page">
       <Header showMenu={false} />
-
-      <main className="order">
-        <div className="container order__content">
-          <h1>Vaše objedávnka</h1>
-          <p className="empty-order">Zatím nemáte nic objednáno</p>
-          <div className="order__items">
-            <div className="order-item">
-              <img
-                src="/cups/espresso.png"
-                className="order-item__image"
-              />
-              <div className="order-item__name">
-                Espresso
-              </div>
-            </div>
-
-            <div className="order-item">
-              <img
-                src="/cups/doppio.png"
-                className="order-item__image"
-              />
-              <div className="order-item__name">
-                Doppio
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-
+      <Order items={data.data}  />
       <Footer />
     </div>
   </div>
